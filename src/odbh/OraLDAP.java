e944b5424a0de6dc731425ad1074b815c9cc863c
Лёша <lesha@192.168.1.130>
2015-09-03 22:23:53 +0400

    ci regexp

--- src/odbh/OraLDAP.java
+++ src/odbh/OraLDAP.java
@@ -19,20 +19,23 @@ import java.util.Map;
 
 /**
  *
+ * @author Tribein
  */
 
 public class OraLDAP {
     LdapContext ldapctx;
     HashMap <String,String> tnslines;
+    HashMap <String,String> tnsports;
+    HashMap <String,String> tnsservices;
     Map <String, String> sortedtnslines;
             
-    public int initmyldap(String ldaphost) {
-        if(ldaphost == null){
-            ldaphost = "ldap.example.org";
-        }        
+    public int initmyldap(String ldaphost, String ldapport) {
+        if(ldaphost == null || ldapport==null){
+            return 1;
+        }              
         Hashtable<String,String> env=new Hashtable<>();
         env.put(Context.INITIAL_CONTEXT_FACTORY,"com.sun.jndi.ldap.LdapCtxFactory");
-        env.put(Context.PROVIDER_URL, "ldap://"+ldaphost+":389");
+        env.put(Context.PROVIDER_URL, "ldap://"+ldaphost+":"+ldapport);
         try{
             ldapctx = new javax.naming.ldap.InitialLdapContext(env,null);        
         }catch (Exception e) {
@@ -50,7 +53,6 @@ public class OraLDAP {
         searchctls.setReturningAttributes(returnedattrs);
         searchctls.setSearchScope(SearchControls.SUBTREE_SCOPE);
         HashMap <String,String> result = new HashMap<>();
-//
         try{
             NamingEnumeration<SearchResult> answer = ldapctx.search(searchbase, searchfilter, searchctls);
             while (answer.hasMoreElements()) {
@@ -58,20 +60,6 @@ public class OraLDAP {
                 Attributes attrs = sr.getAttributes();
                 if (attrs != null) {
                     result.put(attrs.get(returnedattrs[0]).toString(),attrs.get(returnedattrs[1]).toString());
-                    //NamingEnumeration<?> ne = attrs.getAll();
-                    //while (ne.hasMore()) {
-                        //Attribute attr = (Attribute) ne.next();
-                        //if (attr.size() == 1) {
-                        //} else {
-                            //HashSet<String> s = new HashSet<String>();
-                            //NamingEnumeration n =  attr.getAll();
-                            //while (n.hasMoreElements()) {
-                                //s.add((String)n.nextElement());
-                            //}
-                            //amap.put(attr.getID(), s);
-                        //}
-                    //}
-                    //ne.close();
                 }
                 ldapctx.close();
             }
@@ -80,20 +68,38 @@ public class OraLDAP {
             return null;
         }
         if ( ! result.isEmpty() ) {
-            Pattern pattern = Pattern.compile("host=([A-Za-z0-9\\.\\-\\_]*)",Pattern.CASE_INSENSITIVE);
+            Pattern hostpattern = Pattern.compile("host=([A-Za-z0-9\\.\\-\\_]+)");
+            Pattern portpattern = Pattern.compile("port=([0-9]{1,5})");
+            Pattern servicepattern = Pattern.compile("sid=([A-Za-z0-9\\-\\_]+)|service_name=([A-Za-z0-9\\-\\_]+)");
             tnslines = new HashMap<>();
+            tnsports = new HashMap<>();
+            tnsservices = new HashMap<>();
             Set<String> keys = result.keySet();
             for (String k : keys){
-                Matcher matcher = pattern.matcher(result.get(k));
-                if (matcher.find()){
-                    tnslines.put(k.substring(3,k.length()).trim(),matcher.group(1));
+                Matcher hostmatcher = hostpattern.matcher(result.get(k));
+                if (hostmatcher.find()){
+                    tnslines.put(k.substring(3,k.length()).trim(),hostmatcher.group(1));
                 }                
+                Matcher portmatcher = portpattern.matcher(result.get(k));
+                if (portmatcher.find()){
+                    tnsports.put(k.substring(3,k.length()).trim(),portmatcher.group(1));
+                }
+                Matcher servicematcher = servicepattern.matcher(result.get(k));
+                if (servicematcher.find()){
+                    tnsservices.put(k.substring(3,k.length()).trim(),((servicematcher.group(1)==null)? servicematcher.group(2) : servicematcher.group(1)));
+                }
             }
             sortedtnslines = new TreeMap<>(tnslines);
         } else {
             System.out.println("No attributes!");
         }
-//
         return sortedtnslines;
     }
+    
+    public Map <String,String> gettnsports(){
+        return tnsports;
+    }    
+    public Map <String,String> gettnsservices(){
+        return tnsservices;
+    }        
 }
